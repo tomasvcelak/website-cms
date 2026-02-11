85989f5e93aeea60cbf058aeaedea645b97e4aa4
tomasvcelak <vcelak.t@gmail.com>
2026-02-11 10:14:00 +0100

    nwed

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20241125_222020_initial from './20241125_222020_initial';
 import * as migration_20260211_084930 from './20260211_084930';
 import * as migration_20260211_090748 from './20260211_090748';
+import * as migration_20260211_091343 from './20260211_091343';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260211_090748.up,
     down: migration_20260211_090748.down,
-    name: '20260211_090748'
+    name: '20260211_090748',
+  },
+  {
+    up: migration_20260211_091343.up,
+    down: migration_20260211_091343.down,
+    name: '20260211_091343'
   },
 ];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -64,6 +64,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
+    push: process.env.NODE_ENV === 'development',
   }),
   collections: [Pages, Posts, Media, Categories, Users, Comments],
   cors: [getServerSideURL()].filter(Boolean),

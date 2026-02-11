249b879c384015aa948c860017cd9053155930d1
tomasvcelak <vcelak.t@gmail.com>
2026-02-11 09:49:38 +0100

    new mig

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,5 @@
 import * as migration_20241125_222020_initial from './20241125_222020_initial';
-import * as migration_20241214_124128 from './20241214_124128';
+import * as migration_20260211_084930 from './20260211_084930';
 
 export const migrations = [
   {
@@ -8,8 +8,8 @@ export const migrations = [
     name: '20241125_222020_initial',
   },
   {
-    up: migration_20241214_124128.up,
-    down: migration_20241214_124128.down,
-    name: '20241214_124128'
+    up: migration_20260211_084930.up,
+    down: migration_20260211_084930.down,
+    name: '20260211_084930'
   },
 ];

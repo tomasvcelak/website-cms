c9582059e1ef5153c6bacdd9b5f4c8d6bfc96fe6
tomasvcelak <vcelak.t@gmail.com>
2026-02-11 11:11:07 +0100

    hh

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,27 +1,5 @@
 import * as migration_20241125_222020_initial from './20241125_222020_initial';
-import * as migration_20260211_084930 from './20260211_084930';
-import * as migration_20260211_090748 from './20260211_090748';
-import * as migration_20260211_091343 from './20260211_091343';
 
 export const migrations = [
-  {
-    up: migration_20241125_222020_initial.up,
-    down: migration_20241125_222020_initial.down,
-    name: '20241125_222020_initial',
-  },
-  {
-    up: migration_20260211_084930.up,
-    down: migration_20260211_084930.down,
-    name: '20260211_084930',
-  },
-  {
-    up: migration_20260211_090748.up,
-    down: migration_20260211_090748.down,
-    name: '20260211_090748',
-  },
-  {
-    up: migration_20260211_091343.up,
-    down: migration_20260211_091343.down,
-    name: '20260211_091343'
-  },
+
 ];
